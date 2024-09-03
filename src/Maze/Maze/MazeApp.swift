c3d6a9b8d94a6305b9ab2f697b88403d6f765e5c
Janiecee <janiecee@student.21-school.ru>
2024-09-03 11:00:03 +0300

    Добален генератор палочек по массиву из булевых значений

---
+++ src/Maze/Maze/MazeApp.swift
@@ -0,0 +1,18 @@
+//
+//  MazeApp.swift
+//  Maze
+//
+//  Created by Janiece Eleonour on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MazeApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
