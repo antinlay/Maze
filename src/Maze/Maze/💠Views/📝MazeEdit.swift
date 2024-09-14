0f24dc6da8a0a74af7cda66177cdb683910729d2
Janiecee <janiecee@student.21-school.ru>
2024-09-14 19:07:55 +0300

    Исправлен экран редактирования

--- src/Maze/Maze/💠Views/📝MazeEdit.swift
+++ src/Maze/Maze/💠Views/📝MazeEdit.swift
@@ -9,15 +9,33 @@ import SwiftUI
 import PhotosUI
 import SwiftData
 
+enum ImportErrorSaver: LocalizedError {
+    case parseError(String)
+    case fileNotFound(String)
+    case invalidData(String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .parseError(let message):
+            "Parse error: \(message)"
+        case .fileNotFound(let fileName):
+            "File not found: \(fileName)"
+        case .invalidData(let message):
+            "Invalid data: \(message)"
+        }
+    }
+}
+
 struct MazeEdit: View {
     @Environment(\.dismiss) private var dismiss
-    @Environment(\.modelContext) private var context
+    @Environment(\.modelContext) private var modelContext
     @State var maze: MazeRectangular
     @State private var selectedItem: PhotosPickerItem?
     
     @State private var text = ""
-    @State private var error: Error?
+    @State private var error: ImportErrorSaver? = nil
     @State private var isImporting = false
+    @State private var showAlert = false
     
     
     private var importButton: some View {
@@ -35,13 +53,13 @@ struct MazeEdit: View {
             matching: .images,
             photoLibrary: .shared()
         ) {
-#if os(iOS)
+#if os(macOS)
+            Text("Change image")
+#else
             // Display the current image
             MazeImage(maze: maze)
                 .clipShape(.rect(cornerRadius: 20))
                 .padding(.horizontal)
-#else
-            Text("Change image")
 #endif
         }
         .onChange(of: selectedItem) { _, newItem in
@@ -55,7 +73,7 @@ struct MazeEdit: View {
     }
     
     private func changeWalls(walls: Binding<[[Bool]]>) -> some View {
-        ScrollView(.horizontal) {
+        ScrollView([.horizontal, .vertical]) {
             ForEach(walls.wrappedValue.indices, id: \.self) { row in
                 HStack {
                     ForEach(walls.wrappedValue[row].indices, id: \.self) { column in
@@ -63,7 +81,12 @@ struct MazeEdit: View {
                             get: { walls.wrappedValue[row][column] },
                             set: { walls.wrappedValue[row][column] = $0 }
                         )) {
-                            EmptyView()
+                            VStack(alignment: .leading) {
+                                Text(String(format: "%02d", row))
+                                Text(String(format: "%02d", column))
+                            }
+                            .font(.system(size: 8))
+                            .fontWidth(.compressed)
                         }
                     }
                 }
@@ -72,34 +95,38 @@ struct MazeEdit: View {
     }
     
     var body: some View {
-        ScrollView {
-            Text("Edit: \(maze.name)")
+        Form {
+            Section("Name:") {
+                TextField(text: $maze.name) {
+                    //
+                }
                 .font(.headline)
-                .padding(.bottom)
-#if os(macOS)
-            MazeImage(maze: maze)
-                .clipShape(.rect(cornerRadius: 20))
-                .padding(.horizontal)
-#endif
-            imagePicker
-            Divider()
-            
-            //            ScrollView(.horizontal, showsIndicators: false) {
+            }
+            Section("Image:") {
+                imagePicker
+            }
             // Display and edit right walls
-            Text("Right Walls")
-                .font(.headline)
-            changeWalls(walls: $maze.rightWalls)
-            Divider()
+            Section("Right Walls") {
+                changeWalls(walls: $maze.rightWalls)
+            }
             // Display and edit lower walls
-            Text("Lower Walls")
-                .font(.headline)
-            changeWalls(walls: $maze.lowerWalls)
-            Divider()
-            //            }
-                .padding(10)
+            Section("Lower Walls") {
+                changeWalls(walls: $maze.lowerWalls)
+            }
+            .padding(10)
         }
         .toolbar {
             importButton
+        }
+        .alert(isPresented: $showAlert, error: error) { _ in
+            Button("OK") {
+                showAlert = false
+            }
+        } message: { error in
+            Text(error.recoverySuggestion ?? "Try again later.")
+        }
+        .onDisappear {
+            try? modelContext.save()
         }
         .fileImporter(isPresented: $isImporting,
                       allowedContentTypes: [.text]) {
@@ -107,10 +134,21 @@ struct MazeEdit: View {
                 read(from: url)
             }
             switch result {
-            case .success(let text):
-                self.text += text
-            case .failure(let error):
-                self.error = error
+            case .success(let strings):
+                text += strings
+                
+                let parseResult = ParseMazeFiles(str: text).parse()
+                
+                switch parseResult {
+                case .success(let success):
+                    maze.rightWalls = success.verticalWalls
+                    maze.lowerWalls = success.horizontalWalls
+                case .failure(let failure):
+                    error = .parseError(failure.localizedDescription)
+                }
+                
+            case .failure(let failure):
+                error = .invalidData(failure.localizedDescription)
             }
         }
     }
